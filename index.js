b03746bdb1d46b0c7b1b966b1712b5fd4016dbe8
Cyril <cy_one2000@yahoo.fr>
2023-12-08 16:09:43 +0100

    variable plus precises

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const canvas = document.getElementById('Matrix');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("Matrix");
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const nums = '0123456789';
+const katakana =
+  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0123456789";
 
 const alphabet = katakana + latin + nums;
 
 const fontSize = 16;
-const columns = canvas.width/fontSize;
+const columns = canvas.width / fontSize;
 
 const rainDrops = [];
 
-for( let x = 0; x < columns; x++ ) {
-	rainDrops[x] = 1;
+for (let x = 0; x < columns; x++) {
+  rainDrops[x] = 1;
 }
+const drawLine = () => {
+  context.fillStyle = "rgba(0, 0, 0, 0.05)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-const draw = () => {
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	context.fillStyle = '#0F0';
-	context.font = fontSize + 'px monospace';
+  context.fillStyle = "#0F0";
+  context.font = fontSize + "px monospace";
 
-	for(let i = 0; i < rainDrops.length; i++)
-	{
-		const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
-		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
-			rainDrops[i] = 0;
-        }
-		rainDrops[i]++;
-	}
+  for (let i = 0; i < rainDrops.length; i++) {
+    const randomChar = alphabet.charAt(
+      Math.floor(Math.random() * alphabet.length)
+    );
+    context.fillText(randomChar, i * fontSize, rainDrops[i] * fontSize);
+
+    if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      rainDrops[i] = 0;
+    }
+    rainDrops[i]++;
+  }
 };
 
-setInterval(draw, 50);
+setInterval(drawLine, 50);
